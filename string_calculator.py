0884f1c0925ff00efc59cc2f210da0305496a954
djouhaina-chelbi <djuhaynach2023@gmail.com>
2025-10-29 10:47:04 +0100

    cod_3

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,9 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    parts = numbers.split(",")
+    total = 0
+    for num in parts:
+        total += int(num)
+    return total
+
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,4 +1,10 @@
+@ pytest.mark.params([("",0),(1,0,1),(1,2,3)])
+
 def test_add_empty_string():
     assert add("") == 0
+def test_add_1_number():
+    assert add("1") == 1
 
+def test_add_2_numbers():
+    assert add("1,2") == 3
 
